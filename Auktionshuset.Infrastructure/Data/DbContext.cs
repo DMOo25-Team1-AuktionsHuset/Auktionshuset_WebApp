f6a8f90404108d5fa1706b3c11ca11feedaaa152
Patrick Kokholm <patrickkokholm@gmail.com>
2026-09-14 12:33:53 +0200

    Created db context

---
+++ Auktionshuset.Infrastructure/Data/DbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Auktionshuset.Domain.Entities;
+
+namespace Auktionshuset.Infrastructure.Data {
+    public class DbContext : IdentityDbContext {
+        public DbContext(DbContextOptions<DbContext> options) : base(options) {
+        }
+
+        public DbSet<AuctionHouse> AuctionHouse => Set<AuctionHouse>();
+        public DbSet<Lot> Lot => Set<Lot>();
+
+        protected override void OnModelCreating(ModelBuilder builder) {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfigurationsFromAssembly(typeof(DbContext).Assembly);
+        }
+    }
+}
