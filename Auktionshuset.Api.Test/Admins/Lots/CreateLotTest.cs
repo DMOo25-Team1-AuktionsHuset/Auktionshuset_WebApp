da675aacdafe02a409800d9d9a1462ce2261ccc3
Patrick Kokholm <patrickkokholm@gmail.com>
2026-09-14 10:19:11 +0200

    Created Api test project

---
+++ Auktionshuset.Api.Test/Admins/Lots/CreateLotTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Auktionshuset.Api.Test.Admins.Lots {
+    public class CreateLotTest {
+        [Fact]
+        public void Test1() {
+
+        }
+    }
+}
---
+++ Auktionshuset.Api.Test/UnitTest1.cs
@@ -0,0 +1,8 @@
+﻿namespace Auktionshuset.Api.Test {
+    public class UnitTest1 {
+        [Fact]
+        public void Test1() {
+
+        }
+    }
+}
