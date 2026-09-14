54656cb515a484f27d1a6e27de6c4adb1376a71a
Patrick Kokholm <patrickkokholm@gmail.com>
2026-09-14 12:17:20 +0200

    Created infrastructure ef core configuration for Auction House.

--- Auktionshuset.Infrastructure/Data/Configuration/AuctionHouseConfiguration.cs
+++ Auktionshuset.Infrastructure/Data/Configuration/AuctionHouseConfiguration.cs
@@ -5,12 +5,18 @@ using Auktionshuset.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-namespace Auktionshuset.Infrastructure.Data.Configuration {
-    public class AuctionHouseConfiguration : IEntityTypeConfiguration<AuctionHouse> {
-        public void Configure(EntityTypeBuilder<AuctionHouse> entity) {
+namespace Auktionshuset.Infrastructure.Data.Configuration 
+{
+    public class AuctionHouseConfiguration : IEntityTypeConfiguration<AuctionHouse> 
+    {
+        public void Configure(EntityTypeBuilder<AuctionHouse> entity) 
+        {
             entity.HasKey(ah => new { ah.AuctionHouseId });
-
-            entity.Property(ah => ah.AuctionHouseName);
+            entity.Property(ah => ah.AuctionHouseName).IsRequired();
+            entity.Property(ah => ah.Address).IsRequired();
+            entity.Property(ah => ah.CVRNumber).IsRequired();
+            entity.Property(ah => ah.PhoneNumber).IsRequired();
+            entity.Property(ah => ah.Email).IsRequired();
         }
     }
 }
