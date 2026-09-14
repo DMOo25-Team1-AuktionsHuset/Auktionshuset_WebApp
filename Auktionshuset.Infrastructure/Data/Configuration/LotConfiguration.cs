0b99e5c4bfe1676dcc0941ec4a6b31388e25506b
megan <->
2026-09-14 12:25:01 +0200

    Created lotconfiguration

--- Auktionshuset.Infrastructure/Data/Configuration/LotConfiguration.cs
+++ Auktionshuset.Infrastructure/Data/Configuration/LotConfiguration.cs
@@ -1,8 +1,28 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Auktionshuset.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Auktionshuset.Infrastructure.Data.Configuration {
-    internal class LotConfiguration {
+    public class LotConfiguration : IEntityTypeConfiguration<Lot>
+    {
+        public void Configure(EntityTypeBuilder<Lot> entity)
+        {
+            entity.HasKey(l => l.LotId);
+            entity.Property(l => l.Name).IsRequired();
+            entity.Property(l => l.Category).IsRequired();
+            entity.Property(l => l.Quantity).IsRequired();
+            entity.Property(l => l.EstimatedValue).IsRequired();
+            entity.Property(l => l.Description).IsRequired();
+            entity.Property(l => l.Tags).IsRequired();
+
+            entity.HasOne(l => l.AuctionHouse)
+                .WithMany(ah => ah.Lots)
+                .HasForeignKey(l => l.AuctionHouseId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
