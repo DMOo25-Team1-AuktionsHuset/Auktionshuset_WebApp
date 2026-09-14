20669bef7000e3efcaab92a504e46b23248bad25
Patrick Kokholm <patrickkokholm@gmail.com>
2026-09-14 12:12:56 +0200

    Started creating EF Core configurations

---
+++ Auktionshuset.Infrastructure/Data/Configuration/AuctionHouseConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Auktionshuset.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Auktionshuset.Infrastructure.Data.Configuration {
+    public class AuctionHouseConfiguration : IEntityTypeConfiguration<AuctionHouse> {
+        public void Configure(EntityTypeBuilder<AuctionHouse> entity) {
+            entity.HasKey(ah => new { ah.AuctionHouseId });
+
+            entity.Property(ah => ah.AuctionHouseName);
+        }
+    }
+}
---
+++ Auktionshuset.Infrastructure/Data/Configuration/LotConfiguration.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Auktionshuset.Infrastructure.Data.Configuration {
+    internal class LotConfiguration {
+    }
+}
