48c6d278f71693c3dadcc7c43dec76699ba102e1
megan <->
2026-09-17 11:00:11 +0200

    Updated consumerlogic and test

--- Auktionshuset.Infrastructure.Test/Admins/Lots/LotCreatedEventTest.cs
+++ Auktionshuset.Infrastructure.Test/Admins/Lots/LotCreatedEventTest.cs
@@ -10,7 +10,6 @@ using System.Collections.Generic;
 using System.Text;
 using System.Text.Json;
 using Auktionshuset.Application.Admin.Lots.CreateLot;
-using Auktionshuset.Infrastructure.Messaging.Consumers.Lot;
 
 namespace Auktionshuset.Infrastructure.Test.Admins.Lots
 {
@@ -94,7 +93,7 @@ namespace Auktionshuset.Infrastructure.Test.Admins.Lots
         }
 
         [Fact]
-        public async Task Consumer_CallsHandler_WhenLotCreatedEventIsReceived()
+        public async Task AdminConsumer_CallsLotCreatedHandler_WhenLotCreatedEventIsReceived()
         {
             var receivedEvent =
                 new TaskCompletionSource<LotCreatedIntegrationEvent>(
@@ -159,8 +158,8 @@ namespace Auktionshuset.Infrastructure.Test.Admins.Lots
 
             await publishChannel.QueuePurgeAsync(queueName);
 
-        using var consumer =
-            new LotCreatedConsumer(connection, scopeFactory);
+            using var consumer =
+                new AdminEventsConsumer(connection, scopeFactory);
 
             using var cancellationSource =
                 new CancellationTokenSource(TimeSpan.FromSeconds(10));
